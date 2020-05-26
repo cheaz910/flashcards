390089faee0e50a54cbc703370044c5fd53af1b5
AAKiselyova <anyuta_kis_99@mail.ru>
2020-05-26 14:18:44 +0500

    changed set page and added component tableRow

--- Flashcards/ClientApp/src/components/SetPage/index.js
+++ Flashcards/ClientApp/src/components/SetPage/index.js
@@ -3,8 +3,10 @@ import {Fragment} from 'react';
 
 import { userService, authenticationService } from '../../_services';
 import config from "../../config";
-import {authHeader, handleResponse, mergeClassNames} from "../../_helpers";
+import {authHeader, handleResponse} from "../../_helpers";
+import {TableRow} from '../TableRow';
 import styles from './setPage.module.css';
+
 
 export class SetPage extends React.Component {
     constructor(props) {
@@ -16,18 +18,6 @@ export class SetPage extends React.Component {
         };
     }
 
-    getSet(set) {
-        let cards = [];
-        for (let i = 0; i < set.cards.length; i++) {
-            let item = set.cards[i];
-            item['number'] = i;
-            item['isChange'] = false;
-            cards.push(item);
-        }
-        set.cards = cards;
-        return set;
-    }
-
     componentDidMount() {
         const requestOptions = { method: 'POST', headers: {...authHeader(), 'Content-Type': 'application/json'}, body: JSON.stringify({
                 userId: this.state.currentUser.id,
@@ -36,61 +26,31 @@ export class SetPage extends React.Component {
         fetch(`${config.apiUrl}/api/set/1`, requestOptions).then(handleResponse).then(data => this.setState({ set: this.getSet(data.set), nRow: data.set.length, loaded: true }));
     }
 
-    createMutableRow(card) {
-        return (
-            <tr key={card.number}>
-                <th>
-                    <input type="text" defaultValue={card.wordEn}/>
-                </th>
-                <th>
-                    <input type="text" defaultValue={card.wordRu}/>
-                </th>
-                <th>
-                    <button type="button" onClick={() => this.save(card.number)}>Сохранить</button>
-                </th>
-            </tr>
-        );
-    }
-
-    createImmutableRow(card) {
-        return (
-            <tr key={card.number}>
-                <th>{card.wordEn}</th>
-                <th>{card.wordRu}</th>
-                <th>
-                    <button type="button" onClick={() => this.change(card.number)}>Изменить</button>
-                </th>
-            </tr>
-        );
-    }
-
-    save(number) {
-        let newState = this.state;
-        let row = document.querySelectorAll('tr')[number].children;
-        let wordEn = row[0].children[0];
-        let wordRu = row[1].children[0];
-        if (wordEn.value !== "" && wordRu.value !== "") {
-            newState.set.cards[number].isChange = false;
-            if (wordEn.value !== wordEn.defaultValue || wordRu.value !== wordRu.defaultValue) {
-                newState.set.cards[number].wordEn = wordEn.value;
-                newState.set.cards[number].wordRu = wordRu.value;
-                //отправка слова на сервер cards[number]
-            }
-            this.setState(newState);
+    render() {
+        if (!this.state.loaded) {
+            return <h1>loading...</h1>;
         }
-    }
-
-    change(number) {
-        let newState = this.state;
-        newState.set.cards[number].isChange = true;
-        this.setState(newState);
+        return (
+            <Fragment>
+                <h1>
+                    {
+                        this.props.match.params.setId ? `Набор - ${this.props.match.params.setId}` : `Новый набор`
+                    }
+                </h1>
+                <table>
+                    {
+                        this.state.set.cards.map(card => <TableRow card={card}/>)
+                    }
+                </table>
+                <button type="button" onClick={() => this.addRow()}>+</button>
+            </Fragment>
+        );
     }
 
     addRow() {
         let card = {
             id:null,
-            number: this.state.set.cards.length,
-            isChange: true,
+            isMutable: true,
             wordEn:"",
             wordRu:""
         };
@@ -99,24 +59,14 @@ export class SetPage extends React.Component {
         this.setState(newState);
     }
 
-    render() {
-        if (!this.state.loaded) {
-            return <h1>loading...</h1>;
+    getSet(set) {
+        let cards = [];
+        for (let i = 0; i < set.cards.length; i++) {
+            let item = set.cards[i];
+            item['isMutable'] = false;
+            cards.push(item);
         }
-        return (
-            <Fragment>
-                <h1>{
-                    this.props.match.params.setId ? `Набор - ${this.props.match.params.setId}` : `Новый набор`
-                }</h1>
-                <table id="table">
-                    {
-                        this.state.set.cards.map(card =>
-                            (card.isChange) ? this.createMutableRow(card) : this.createImmutableRow(card)
-                        )
-                    }
-                </table>
-                <button type="button" onClick={this.addRow}>+</button>
-            </Fragment>
-        );
+        set.cards = cards;
+        return set;
     }
 }
